8bd6937967aec6c3bd6305019fbe29045b2f9392
Orudnev <olrudnev@gmail.com>
2024-10-29 18:09:41 +0600

    Data-tree  Add,Delete,Edit elements

--- src/Components/DataTree/DTItems/index.tsx
+++ src/Components/DataTree/DTItems/index.tsx
@@ -2,16 +2,49 @@ import { TreeItem } from "react-complex-tree";
 import { ITreeItemData } from "../../test-tree";
 import { IDataTreeProps } from "../component";
 import { DtTest } from "./dt-test";
+import { ToolbarButton } from "../../toolbar-button";
 
-export type TDtItemType = 'test'|'folder'|'leaf';
+export type TEditorType = 'test'|'folder'|'leaf';
 
-export interface IDtItem{
-    type:TDtItemType;
+export interface IEditorProps {
+    treeItem: TreeItem<ITreeItemData>;
+    onIndexChanged?: (item:TreeItem<ITreeItemData>,newValue:number)=>void;
+    onValueChanged?: (item:TreeItem<ITreeItemData>,newValue:ITreeItemData)=>void;
+}  
+
+export type TDtItemToolbarCommand = 'add'|'delete'|'clearSelection';
+export function GetDtItemToolbar<T>(item:T,allowDelete:boolean,
+    onToolbarButtonClick?:(btnCommand: TDtItemToolbarCommand)=>void){
+    const selectedItem = item as TreeItem<ITreeItemData>; 
+    const handleClick = (btnCmd:TDtItemToolbarCommand) =>{
+        if(onToolbarButtonClick){
+            onToolbarButtonClick(btnCmd);
+        }
+    }
+    let noselection = selectedItem?false:true;
+    if(!selectedItem){
+        return(
+            <div className='dtree_item-toolbar'>
+                <ToolbarButton toolTip='Добавить корневой элемент' image='plus' size='24' class = 'toolbar-button' onClick={()=>{handleClick('add')}} />
+            </div>        
+        );
+    }
+    return(
+        <div className='dtree_item-toolbar'>
+            <ToolbarButton toolTip='Добавить вложенный элемент' image='plus' size='24' class = 'toolbar-button' onClick={()=>{handleClick('add')}} />
+            {allowDelete&&<ToolbarButton toolTip='Удалить выделенный элемент' image='cancel' size='24' class = 'toolbar-button' onClick={()=>{handleClick('delete')}} />}
+            <ToolbarButton toolTip='Сбросить отметку выделенного элемента' image='selitems-off' size='24' class = 'toolbar-button' onClick={()=>{handleClick('clearSelection')}} />
+        </div>        
+    );
 }
 
+export function GetDtItemEditor<T>(dtItemType:TEditorType, genProps:T,
+    onValueChange?:(item:TreeItem<ITreeItemData>,newValue:ITreeItemData)=>void){
+    const props:IEditorProps = {
+        treeItem: genProps as TreeItem<ITreeItemData>,
+        onValueChanged:onValueChange
+    } 
 
-export function GetDtItemEditor<T>(dtItemType:TDtItemType, gprops:T){
-    const props = gprops as TreeItem<ITreeItemData>;
     switch (dtItemType){
         case "test":
             return (<DtTest {...props}/>);
@@ -19,3 +52,4 @@ export function GetDtItemEditor<T>(dtItemType:TDtItemType, gprops:T){
             return (<></>); 
     }
 }
+
--- src/Components/DataTree/component.tsx
+++ src/Components/DataTree/component.tsx
@@ -2,12 +2,12 @@ import { useMemo,useState,useEffect } from 'react';
 import { UncontrolledTreeEnvironment,ControlledTreeEnvironment, Tree, StaticTreeDataProvider, TreeItemIndex, TreeDataProvider,TreeItem } from 'react-complex-tree';
 import 'react-complex-tree/lib/style-modern.css';
 import './data-tree.css';
-import { GetDtItemEditor,TDtItemType } from './DTItems';
-import { ToolbarButton } from '../toolbar-button';
+import { GetDtItemEditor, GetDtItemToolbar } from './DTItems';
+import { ITreeItemData } from '../test-tree';
 
 
 export interface IDataTreeProps<T>{
-    items:TreeItem<T>[]
+    items:TreeItem<T>[];
 }
 
 
@@ -22,24 +22,100 @@ export function DataTree<T>(props:IDataTreeProps<T>){
     }
     const [rows,setRows] = useState(convertItemsToRowObject(props.items));
     const dataProvider = useMemo(
-        () =>
-          new StaticTreeDataProvider(rows, (row, data) => ({            
+        () =>{
+          console.log("************** dataprovider **************");
+          return new StaticTreeDataProvider(rows, (row, data) => ({            
             ...row,
             data,
-          })),
+          }))},
         [rows]
       );  
-    const [selItemIdx,setSelItemIdx] = useState("");
+    const [selItemIdx,setSelItemIdx] = useState(0);
     const selectedItem:TreeItem<T> = rows[selItemIdx];
+    const allowDelete = selectedItem && !selectedItem.isFolder && rows['root'].children.length>1;
     return (
       <div className='dtree'>
-        <div className='dtree_item-toolbar'>
-            <ToolbarButton toolTip='' image='plus' size='24' class = 'toolbar-button' onClick={()=>{}} />
-            <ToolbarButton toolTip='' image='plus' size='24' class = 'toolbar-button' onClick={()=>{}} />
-            <ToolbarButton toolTip='' image='plus' size='24' class = 'toolbar-button' onClick={()=>{}} />
-            <ToolbarButton toolTip='' image='plus' size='24' class = 'toolbar-button' onClick={()=>{}} />
-            <ToolbarButton toolTip='' image='plus' size='24' class = 'toolbar-button' onClick={()=>{}} />
-        </div>
+        {GetDtItemToolbar(selectedItem,allowDelete,(cmd)=>{
+          switch(cmd){
+            case 'add':
+                let lastIndex = -1;
+                //Определяем максимальный индекс (для того, чтобы сгенерировать следующий)
+                for(const key in rows){
+                   let row = rows[key] as TreeItem<ITreeItemData>;
+                   if(isNaN(row.index as any)){
+                      continue;
+                   }
+                   let currIndex = row.index as number;
+                   if(currIndex>lastIndex){
+                    lastIndex = currIndex;
+                   }
+                }
+                //генерируем следующий индекс
+                lastIndex++;
+                const emptyData:ITreeItemData = {
+                  operation:'Contains',
+                  label:'New element',
+                  value:''
+                } 
+                let newRow:TreeItem<ITreeItemData> = {
+                  index:lastIndex,
+                  isFolder:false,
+                  data:emptyData,
+                  children:undefined
+                }
+                const newRows = {...rows};
+                newRows[lastIndex] = newRow;
+                let parent = newRows['root'];
+                if (selectedItem){
+                  parent = newRows[selectedItem.index];
+                }
+                if(parent.isFolder){
+                  parent.children?.push(lastIndex);
+                } else {
+                    parent.children = [lastIndex];
+                }
+                parent.isFolder = true;
+                setRows(newRows);
+                break;
+            case 'delete':
+              const nr:any = {};
+              let newSelectedIndex = -1;
+              for(const key in rows){
+                let row:TreeItem<ITreeItemData> = rows[key];
+                if(row.children){
+                  let delIndex = row.children.findIndex(itm=>itm == selectedItem.index);
+                  if(delIndex>-1){
+                    let parent:TreeItem<ITreeItemData> = row;
+                    if(parent.children){
+                      parent.children.splice(delIndex,1);
+                      if(parent.children.length>0){
+                        if(parent.children.length>delIndex){
+                          newSelectedIndex = parent.children[delIndex] as number;  
+                        } else {
+                          newSelectedIndex = parent.children[parent.children.length-1] as number;
+                        }                        
+                      } else {
+                        parent.isFolder = false;
+                        newSelectedIndex = parent.index as number;
+                      }
+                    }
+                  }
+                }
+                if(selectedItem.index != key){
+                  nr[key] = row;
+                }
+              }
+              setRows(nr);
+              if(newSelectedIndex>-1){
+                setSelItemIdx(newSelectedIndex);
+              }
+              break;
+            case 'clearSelection':
+              setSelItemIdx(0);
+              break;
+            default: 
+          }
+        })}
         <UncontrolledTreeEnvironment       
             canDragAndDrop
             canDropOnFolder
@@ -52,7 +128,15 @@ export function DataTree<T>(props:IDataTreeProps<T>){
               },
             }}      
             renderItemTitle={({ title,item,context }) =>{
-              return <span>{title}</span>;              
+              let lblText = title;
+              let datat:ITreeItemData = item.data;
+              if(datat && datat.label){
+                lblText = datat.label;
+              }
+              if(item.index === selItemIdx){
+                return <span className='item-title-sel'>{lblText}</span>;                
+              }
+              return <span>{lblText}</span>;              
             }}
             renderItemArrow={({ item, context }) =>{
             if(item.isFolder){
@@ -78,7 +162,7 @@ export function DataTree<T>(props:IDataTreeProps<T>){
             }
            } 
             onFocusItem={(item,treeId,setDomFocus)=>{
-              setSelItemIdx(item.index.toString());
+              setSelItemIdx(item.index as number);
             }}           
             renderItem={({ title, arrow, depth, context, children }) => {
               return (
@@ -99,9 +183,30 @@ export function DataTree<T>(props:IDataTreeProps<T>){
             <Tree treeId="tree-1" rootItem="root" />
         </UncontrolledTreeEnvironment>
         <div className='dtree_item-details'>
-            {GetDtItemEditor('test',selectedItem)}
+            {GetDtItemEditor('test',selectedItem,
+            (item,newValue)=>{
+              item.data = newValue;
+              setRows({...rows});
+            })}
         </div>
       </div>
     );
 }
+
+
+
+// (item,newIndex)=>{
+//   let newRows:any = {};
+//   for(const key in rows){
+//     if(item.index === key){
+//       newRows[newIndex] = rows[key];
+//       newRows[newIndex].index = newIndex;
+//     } else {
+//       newRows[key] = rows[key];                  
+//     }
+//   } 
+//   setRows(newRows);
+//   setSelItemIdx(newIndex);              
+// },
+
 
